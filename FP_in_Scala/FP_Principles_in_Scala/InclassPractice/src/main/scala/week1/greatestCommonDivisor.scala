65a19825b4b751f03ebb06f6f3952b54f8a19b3c
goalkeeperfyc <goalkeeperyucheng@gmail.com>
2020-06-03 23:57:31 -0400

    finish week 1 in class practice

--- FP_in_Scala/FP_Principles_in_Scala/InclassPractice/src/main/scala/week1/greatestCommonDivisor.scala
+++ FP_in_Scala/FP_Principles_in_Scala/InclassPractice/src/main/scala/week1/greatestCommonDivisor.scala
@@ -9,4 +9,12 @@ object greatestCommonDivisor extends App {
 
   println(gcd(14, 21))
   println(gcd(30, 15))
+
+  def factorial(a: Int): Int =
+    if (a == 0) 1
+    else a * factorial(a-1)
+
+  val factorial_result = factorial(4)
+  println(factorial_result)
 }
+
