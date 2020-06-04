0b039366a5d1f566fe732a7789daff030924fe97
goalkeeperfyc <goalkeeperyucheng@gmail.com>
2020-06-03 23:29:29 -0400

    update

--- FP_in_Scala/FP_Principles_in_Scala/InclassPractice/src/main/scala/week1/greatestCommonDivisor.scala
+++ FP_in_Scala/FP_Principles_in_Scala/InclassPractice/src/main/scala/week1/greatestCommonDivisor.scala
@@ -4,5 +4,9 @@ object greatestCommonDivisor extends App {
   def gcd(a: Int, b: Int): Int =
     if (b == 0) a else gcd(b, a % b)
 
+  val gcd_number = gcd(13, 39)
+  println(gcd_number)
+
   println(gcd(14, 21))
+  println(gcd(30, 15))
 }
